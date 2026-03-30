93bda718a7efc069181690031cfd9288bec68f16
Emilio Sala <emilio_k88@hotmail.com>
2026-03-30 10:47:19 +0200

    ADD: main cli initialize

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+function main() {
+  if (process.argv.length !== 3) {
+    console.log("Wrong argument number");
+    process.exit(1);
+  }
+  const baseURL = process.argv[2];
+  console.log("Starting craweler for " + baseURL);
+}
+
+main();
