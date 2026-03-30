e00af87a72ed66db63addd8c44f4cc28335bb196
Emilio Sala <emilio_k88@hotmail.com>
2026-03-30 10:52:53 +0200

    ADD: getHTML from url cli

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
-function main() {
+async function main() {
   if (process.argv.length !== 3) {
     console.log("Wrong argument number");
     process.exit(1);
   }
   const baseURL = process.argv[2];
   console.log("Starting craweler for " + baseURL);
+  try {
+    const text = await getHTML(baseURL);
+    console.log(text);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 }
 
 main();
+
+async function getHTML(url: string) {
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "User-Agent": "BootCrawler/1.0",
+    },
+  });
+  if (response.status !== 200) {
+    throw new Error("Failed to fetched, status code: " + response.status);
+  }
+  if (response.headers.get("content-type") !== "text/html") {
+    throw new Error("Content type is not text/html");
+  }
+
+  return response.text();
+}
